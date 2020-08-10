4134ff5ac6b003bcf7077659cb0e46d5427cf8b4
sunyuliang <sunyuliang@huice.com>
2020-08-10 09:41:48 +0800

    add

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -4,7 +4,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Yudioll cloud Blog</h3>
+        <h3 class="title">Yudioll Cloud Blog</h3>
       </div>
 
       <el-form-item prop="username">
